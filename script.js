af67d9219ad115e2bd64bf071f0e2eca44e9e093
imon hossin <imonhossin104@gmail.com>
2022-04-13 22:21:17 +0600

    first

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
 
-// Initializing some values
-let totalAttempt = 5;
-let attempt = 0;
-let totalWon = 0;
-let totalLost = 0;
+    // Initialize some value
+    let totalAttempts = 5;
+    let attempt = 0;
+    let totalWon = 0;
+    let totalLost = 0;
 
-//  Finding Elements
- const cardBody =  document.querySelector(".card-body");
- const form = document.querySelector('form');
- const guessingNumber = form.querySelector('#guessingNumber');
- const checkButton =  document.querySelector("#check-button");
- const resultText =  document.querySelector(".result-text");
- const remainingAttempt = document.querySelector(".remaining-attempt");
- const p = document.createElement("p");
+    // Finding Elements
+    const cardBody = document.querySelector(".card-body") ;
+    const form = cardBody.querySelector("form");
+    const guessingNumber =  cardBody.querySelector("#guessingNumber");
+    const checkButton = cardBody.querySelector("#check-button");
+    const resultText = cardBody.querySelector(".result-text");
+    const remainingAttempt = cardBody.querySelector(".remaining-attempt");
+    const lostWonMessage =  document.createElement("p");
 
- document.getElementById('check-button').addEventListener("click",function(event){
-     event.preventDefault();
-    //  console.log (typeof guessingNumber.value);
+    checkButton.addEventListener("click", function(event){
+        event.preventDefault();
+        attempt++;
 
-//   console.log(guessingNumber.value)
-attempt++;
-if(attempt===5){
+        if(attempt==5){
     guessingNumber.disabled = true;
     checkButton.disabled = true;
-}if(attempt<6){
-    checkResult(guessingNumber.value)  ;
-    remainingAttempt.innerHTML = `
-    Remaining Attempts : ${totalAttempt-attempt}`;
+        }
+        if(attempt<6){
+            checkResult(guessingNumber.value);
+            remainingAttempt.innerHTML = `
+            Total Attempts: ${totalAttempts-attempt}
+            `;
+        }
 
-}
-  guessingNumber.value = '';
-
- })
-
- function checkResult(guessingNumber){
-    //  console.log(guessingNumber)
-     let randomNumber =  getRandomNumber(5);
-    //  console.log(randomNumber);
-    if( guessingNumber==randomNumber){
-        resultText.innerHTML = `
-        you have won
-        `
-        totalWon++;
-    }else{
-        resultText.innerHTML = `
-        you have lost. and random number was: ${randomNumber}
-        `
-        totalLost++;
-    }
-    p.innerHTML = `
-    won: ${totalWon} Lost: ${totalLost}
+        guessingNumber.value = '';
+    })
+    // check result
+    const checkResult = (guessingNumber) =>{
+        let randomNumber = getRandomNumber(5);
+        if(guessingNumber==randomNumber){
+    resultText.innerHTML=`
+    You have won
     `;
-    p.classList.add("largest-text");
-    cardBody.appendChild(p);
+    totalWon++;
+        }else{
+            resultText.innerHTML = `
+            You have lost random number was: ${randomNumber}
+            `
+            totalLost++;
+        }
+        lostWonMessage.innerHTML = `
+        Won: ${totalWon} Lost: ${totalLost}
+        `
+        lostWonMessage.classList.add("largest-text");
+        cardBody.appendChild(lostWonMessage);
+    }
 
- }
+    // get random number
+    const getRandomNumber = (number)=>{
+        return Math.floor(Math.random()*number) + 1;
 
- function getRandomNumber (number){
-  return  Math.floor(Math.random() *number) +1;
- }
+    }
+    
